8f38a89d06c0d809b27cee6ce6fde03ca09f841f
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-02-17 15:15:00 -0700

    Changed to constructor and outcome

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //String studentName, String className1, String className2, String className3, int grade, int grade2, int grade3
+        Person1 person1 = new Person1("Bob", "Math", "English", "Science", 89, 95, 78);
+        Person2 person2= new Person2("LaiLanni", "Math", "Endlish", "Science", 99, 72, 86);
+
+        // add an array for multiple classes
+        //String[] classes= {"math", "spanish", "English", "Science", "Band", "History"};
+        //student.setStudentName("Bob");
+        //student.setClassName("Math");
+
+
+        //Student to name a class and enter their grade for each class
+        //Scanner nameOfClass = new Scanner(System.in);
+        //for (String nameOfClasses: classes) {
+            //System.out.print("Please Enter a Class: ");
+            //classes = nameOfClasses.nextLine();
+        //}
+
+    person1.describeStudent();
+    person2.describeStudent();
+
+    System.out.print("Who got the better grade in Math?\n");
+    if (person1.getGrade() > person2.getGrade()) {
+        System.out.println(person1.getStudentName() + "got the better grade");
+    } else {
+        System.out.println(person2.getStudentName() + "got the better grade");
+    }
+    /*old code:
         Scanner grade = new Scanner(System.in);
-        System.out.println("Please enter your grade:  ");
+             System.out.println("Please enter your grade:  ");
         int num1 = grade.nextInt();
 
     if (num1 >= 90) {
@@ -18,6 +45,6 @@ public class Main {
         System.out.println("D");
         } else {
         System.out.println("F");
-        }
+        } */
     }
 }
--- src/com/company/Person2.java
+++ src/com/company/Person2.java
@@ -1,2 +1,80 @@
-package com.company;public class Person2 {
+package com.company;
+
+public class Person2 {
+    private String studentName;
+    private String className1;
+    private String className2;
+    private String className3;
+    private int grade;
+    private int grade2;
+    private int grade3;
+
+    public Person2(String studentName, String className1, String className2, String className3, int grade, int grade2, int grade3) {
+        this.studentName = studentName;
+        this.className1= className1;
+        this.className2= className2;
+        this.className3= className3;
+        this.grade = grade;
+        this.grade2 = grade2;
+        this.grade3 = grade3;
+    }
+
+    //set and get all classNames
+    public void setClassName1(String className1) {
+        this.className1 = className1;
+    }
+    public String getClassName1 () {
+        return className1;
+    }
+    public void setClassName2(String className2) {
+        this.className2 = className2;
+    }
+    public String getClassName2 () {
+        return className2;
+    }
+    public void setClassName3(String className3) {
+        this.className3 = className3;
+    }
+    public String getClassName3 () {
+        return className3;
+    }
+
+    //set and get all grades
+
+    public void setGrade (int grade) {
+        this.grade= grade;
+    }
+    public int getGrade() {
+        return grade;
+    }
+    public void setGrade2 (int grade2) {
+        this.grade2= grade2;
+    }
+    public int getGrade2() {
+        return grade2;
+    }
+    public void setGrade3 (int grade3) {
+        this.grade3= grade3;
+    }
+    public int getGrade3() {
+        return grade3;
+    }
+
+
+    public void setStudentName (String nameOfStudent) {
+        studentName = nameOfStudent;
+    }
+
+    public String getStudentName () {
+        return studentName;
+    }
+
+    //method
+    public void describeStudent() {
+        System.out.println("Student: " + getStudentName() + ", " + getClassName1() + ": " + getGrade() +
+                ", " + getClassName2() + ": " + getGrade2() + ", " + getClassName3() + ": " + getGrade3());
+    }
+    public void gradeAverage() {
+        System.out.println("The student's grade point average is " + ((getGrade() + getGrade2() + getGrade3())/3));
+    }
 }
